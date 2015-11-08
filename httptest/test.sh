6109d6827c94a9d3f826863aaec2995fd16cbd1c
fiatjaf@spooner <fiatjaf@gmail.com>
2015-11-08 10:58:15 -0500

    move curl and bash-based tests.

--- httptest/test.sh
+++ httptest/test.sh
@@ -24,11 +24,17 @@ crud () {
   assert "curl http://localhost:5000/frutas/banana/cor" '"amarela"';
   assert "curl http://localhost:5000/frutas/banana/cor/_rev | py -x 'x[0]'" "1";
   assert "curl 'http://localhost:5000/frutas/banana?children=true' | jq '.cor._val'" '"amarela"'
-  curl -X PUT http://localhost:5000/frutas/banana/cor -d '{"_val": "azul"}' -H 'content-type: text/json';
-  assert "curl http://localhost:5000/frutas/banana/cor" '"azul"';
+  curl -X PUT http://localhost:5000/frutas/banana/cor -d '{"_val": "azul", "tipo": "simples"}' -H 'content-type: text/json';
+  assert "curl http://localhost:5000/frutas/banana/cor/tipo" '"simples"';
   assert "curl http://localhost:5000/frutas/banana/cor/_rev | py -x 'x[0]'" "2";
   assert "curl 'http://localhost:5000/frutas/banana?children=true' | jq '.cor._val'" '"azul"'
   curl -X PUT http://localhost:5000/frutas/banana -d '{"carboidratos": 23}' -H 'content-type: text/json';
+
+  assert "curl http://localhost:5000/frutas/banana/_rev | py -x 'x[0]'" "2";
+  curl -X PUT http://localhost:5000/frutas/banana/quant -d '{"_val": 23}' -H 'content-type: text/json';
+  assert "curl http://localhost:5000/frutas/banana/cor/_rev | py -x 'x[0]'" "2";
+  assert "curl http://localhost:5000/frutas/banana/quant/_rev | py -x 'x[0]'" "1";
+  assert "curl http://localhost:5000/frutas/banana/_rev | py -x 'x[0]'" "3";
 
   assert_end
 }
