5b32835a4526297447f276e5519ddba87876dd13
fiatjaf@spooner <fiatjaf@gmail.com>
2015-11-14 18:33:40 -0500

    modify things so travis will work.

--- views/map.go
+++ views/map.go
@@ -1,32 +1,33 @@
 package views
 
-import (
-	"github.com/robertkrimen/otto"
-	"log"
-
-	"github.com/fiatjaf/summadb/database"
-)
-
-func Map(viewName string, key string, val map[string]interface{}) {
-	// get javascript defined function as a string
-	js, err := database.GetValue("/_map/" + viewName)
-	if err != nil {
-		log.Print("couldn't find map function " + viewName)
-		return
-	}
-
-	// run it
-	vm := otto.New()
-	vm.Set("emit", func(call otto.FunctionCall) otto.Value {
-		key := call.Argument(0)
-		value := call.Argument(1)
-
-		// indexing happens here
-
-		return otto.Value{}
-	})
-	_, err = vm.Call(string(js), map[string]interface{}, map[string]interface{}{
-		"_id":  key,
-		"_val": val,
-	})
-}
+//import (
+//	"log"
+//
+//	"github.com/robertkrimen/otto"
+//
+//	"github.com/fiatjaf/summadb/database"
+//)
+//
+//func Map(viewName string, key string, val map[string]interface{}) {
+//	// get javascript defined function as a string
+//	js, err := database.GetValue("/_map/" + viewName)
+//	if err != nil {
+//		log.Print("couldn't find map function " + viewName)
+//		return
+//	}
+//
+//	// run it
+//	vm := otto.New()
+//	vm.Set("emit", func(call otto.FunctionCall) otto.Value {
+//		key := call.Argument(0)
+//		value := call.Argument(1)
+//
+//		// indexing happens here
+//
+//		return otto.Value{}
+//	})
+//	_, err = vm.Call(string(js), map[string]interface{}, map[string]interface{}{
+//		"_id":  key,
+//		"_val": val,
+//	})
+//}
