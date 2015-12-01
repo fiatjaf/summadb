ff9acef103df6976fbdebf7fa8e8316747661d51
fiatjaf@spooner <fiatjaf@gmail.com>
2015-12-01 17:48:02 -0500

    more pouchdb replication tests, not yet passing.

--- integration_tests/test.js
+++ integration_tests/test.js
@@ -1,4 +1,10 @@
-var expect = chai.expect
+if (typeof window == 'undefined') {
+  expect = require('chai').expect
+  PouchDB = require('pouchdb')
+  fetch = require('node-fetch')
+} else {
+  expect = chai.expect
+}
 
 var local
 var summa = "http://spooner.alhur.es:5000/subdb"
@@ -44,6 +50,64 @@ describe('integration', function () {
       }).then((doc) => {
         expect(doc).to.have.all.keys(['_id', '_rev', 'what'])
         expect(doc.what).to.deep.equal(value('a doc'))
+      })
+    })
+
+    it('should replicate from pouchdb to summa root', () => {
+      var revs = []
+
+      return Promise.resolve().then(()=> {
+        return local.bulkDocs([
+          {_id: 'this', sub: 'this is a document'},
+          {_id: 'that', sub: 'that is a document'},
+          {_id: 'array', array: [1,2,3,4,5]},
+          {_id: 'complex', array: [
+            ['a', {letter: 'a'}],
+            {'subarray': [
+              1, 2, ['xxx']
+            ]
+          }, true, 5]}
+        ])
+      }).then((res) => {
+        revs = res.map(r => r.rev)
+        return PouchDB.replicate(local, summa)
+      }).then(() => {
+        return Promise.all([
+          fetch(summa + '/that').then(r => r.json()),
+          fetch(summa + '/array').then(r => r.json()),
+          fetch(summa + '/complex').then(r => r.json()),
+        ]).then(that, array, complex => {
+          expect(that._id).to.equal('that')
+          expect(that._rev).to.equal(revs[1])
+          expect(array._id).to.equal('array')
+          expect(array._rev).to.equal(revs[2])
+          expect(array.array).to.deep.equal({
+            '0': value(1),
+            '1': value(2),
+            '2': value(3),
+            '3': value(4),
+            '4': value(5),
+          })
+          expect(complex.array).to.deep.equal({
+            '0': {
+              '0': value('a'),
+              '1': {
+                letter: value('a')
+              }
+            },
+            '1': {
+              subarray: {
+                '0': value(1),
+                '1': value(2),
+                '2': {
+                  '0': value('xxx')
+                }
+              }
+            },
+            '2': value(true),
+            '3': value(5)
+          })
+        })
       })
     })
   })
